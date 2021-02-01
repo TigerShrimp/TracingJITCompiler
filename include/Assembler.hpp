de3365372c6afe5079da09293c7a4b45db336905
Simon Kärrman <simonkarrman@icloud.com>
2021-02-01 16:39:36 +0100

    Add first simple instructions to the assembler using asmjit

--- include/Assembler.hpp
+++ include/Assembler.hpp
@@ -1,18 +1,37 @@
 #ifndef ASSEMBLER_HPP
 #define ASSEMBLER_HPP
+#include <asmjit/asmjit.h>
+
 #include <algorithm>
 #include <iostream>
+#include <regex>
 #include <sstream>
 #include <string>
 #include <vector>
 
-#include "HexHelper.hpp"
+static const std::regex regReg(
+    "RDI|RSI|RDX|RCX|R8|R9|R10|R11|RAX|RBX|R12|R13|R14|R15");
+static const std::regex lowRegReg("RDI|RSI|RDX|RCX|RAX|RBX|RSP|RBP");
+static const std::regex memReg("\\[RBP\\+[0-9]+\\]");
+static const std::regex immReg("^[0-9]+$");
+
+typedef int (*Func)(void);
+enum ArgumentType { Reg, Mem, Imm };
+
+struct Argument {
+  ArgumentType type;
+  std::string val;
+};
 
 class Assembler {
  private:
  public:
-  std::vector<hexCode> assemble(std::vector<std::string>&);
-  std::vector<hexCode> assemble(std::vector<Instruction>&);
+  void assemble(std::vector<std::string>&);
 };
+
+static const ArgumentType getArgumentType(std::string argument);
+static const asmjit::x86::Gp getReg(std::string regString);
+static const Argument getArgument(std::string argString);
+static const asmjit::Operand convert(Argument arg);
 
 #endif  // ASSEMBLER_HPP
--- src/Assembler.cpp
+++ src/Assembler.cpp
@@ -1,11 +1,17 @@
 #include "Assembler.hpp"
 
 using namespace std;
+using namespace asmjit;
+
 // Private functions
 
 // Public functions
-vector<hexCode> Assembler::assemble(vector<string> &asmRows) {
-  vector<Instruction> instructionStream;
+void Assembler::assemble(vector<string>& asmRows) {
+  JitRuntime runtime;
+  CodeHolder codeHolder;
+  codeHolder.init(runtime.environment());
+  x86::Assembler asmAssembler(&codeHolder);
+
   for (auto asmRow : asmRows) {
     transform(asmRow.begin(), asmRow.end(), asmRow.begin(), ::toupper);
     replace(asmRow.begin(), asmRow.end(), ',', ' ');
@@ -17,26 +23,125 @@ vector<hexCode> Assembler::assemble(vector<string> &asmRows) {
         instParts.push_back(part);
       }
     }
-    Instruction instruction = {Hex::getMnemonic(instParts[0]), {None}, {None}};
-    switch (instParts.size()) {
-      case 3:
-        instruction.op2 = Hex::getOp(instParts[2]);
+    int operands = instParts.size() - 1;
+
+    Operand op1, op2;
+    switch (operands) {
       case 2:
-        instruction.op1 = Hex::getOp(instParts[1]);
+        op2 = convert(getArgument(instParts[2]));
+      case 1:
+        op1 = convert(getArgument(instParts[1]));
         break;
     }
-    instructionStream.push_back(instruction);
-  }
-  return this->assemble(instructionStream);
-}
+    string mnemonic = instParts[0];
+    x86::Inst::Id inst;
 
-vector<hexCode> Assembler::assemble(vector<Instruction> &instructionStream) {
-  vector<hexCode> hexStream;
-  for (auto instruction : instructionStream) {
-    hexCode mode = Hex::getMode(instruction);
-    if (mode != 0) {
-      hexStream.push_back(mode);
+    if (mnemonic == "MOV") {
+      inst = x86::Inst::kIdMov;
+    } else if (mnemonic == "ADD") {
+      inst = x86::Inst::kIdAdd;
+    } else if (mnemonic == "ENTER") {
+      inst = x86::Inst::kIdEnter;
+    } else if (mnemonic == "LEAVE") {
+      inst = x86::Inst::kIdLeave;
+    } else if (mnemonic == "RET") {
+      inst = x86::Inst::kIdRet;
+    }
+
+    switch (operands) {
+      case 2:
+        asmAssembler.emit(inst, op1, op2);
+        break;
+      case 1:
+        asmAssembler.emit(inst, op1);
+        break;
+      default:
+        asmAssembler.emit(inst);
+        break;
     }
   }
-  return hexStream;
+  Func fn;
+  Error err = runtime.add(&fn, &codeHolder);
+  if (err) {
+    cout << "Compilen't";
+    perror("");
+    exit(EXIT_FAILURE);
+  }
+  int res = fn();
+  cout << "Res: " << res << endl;
+  runtime.release(fn);
+}
+
+static const ArgumentType getArgumentType(string argument) {
+  ArgumentType arg;
+  if (regex_match(argument.begin(), argument.end(), immReg)) {
+    arg = ArgumentType::Imm;
+  } else if (regex_match(argument.begin(), argument.end(), memReg)) {
+    arg = ArgumentType::Mem;
+  } else if (regex_match(argument.begin(), argument.end(), regReg)) {
+    arg = ArgumentType::Reg;
+  } else {
+    throw;
+  }
+  return arg;
+}
+
+static const x86::Gp getReg(std::string regString) {
+  if (regString == "RAX") {
+    return x86::rax;
+  } else if (regString == "RCX") {
+    return x86::rcx;
+  } else if (regString == "RDX") {
+    return x86::rdx;
+  } else if (regString == "RBX") {
+    return x86::rbx;
+  } else if (regString == "RSP") {
+    return x86::rsp;
+  } else if (regString == "RBP") {
+    return x86::rbp;
+  } else if (regString == "RSI") {
+    return x86::rsi;
+  } else if (regString == "RDI") {
+    return x86::rdi;
+  } else if (regString == "R8") {
+    return x86::r8;
+  } else if (regString == "R9") {
+    return x86::r9;
+  } else if (regString == "R10") {
+    return x86::r10;
+  } else if (regString == "R11") {
+    return x86::r11;
+  } else if (regString == "R12") {
+    return x86::r12;
+  } else if (regString == "R13") {
+    return x86::r13;
+  } else if (regString == "R14") {
+    return x86::r14;
+  } else {  // R15
+    return x86::r15;
+  }
+}
+
+static const Argument getArgument(string argString) {
+  ArgumentType t = getArgumentType(argString);
+  Argument arg = {t, argString};
+  return arg;
+}
+
+static const Operand convert(Argument arg) {
+  switch (arg.type) {
+    case ArgumentType::Reg: {
+      x86::Gp reg = getReg(arg.val);
+      return reg;
+    }
+    case ArgumentType::Mem: {
+      x86::Mem mem = x86::ptr(x86::rbp, 8);
+      return mem;
+    }
+    case ArgumentType::Imm: {
+      asmjit::Imm imm;
+      imm.setValue(stoi(arg.val));
+      return imm;
+    }
+  }
 }
