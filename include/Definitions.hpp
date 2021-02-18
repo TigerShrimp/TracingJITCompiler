6df0c7a97f01e19d0e0fb996c28aa19b7786336c
Jakob Erlandsson <erjakob@student.chalmers.se>
2021-02-18 17:04:40 +0100

    prepare for debig as stream instead of cout

--- include/Definitions.hpp
+++ include/Definitions.hpp
@@ -1,6 +1,9 @@
 #ifndef DEFINITIONS_HPP
 #define DEFINITIONS_HPP
+#include <iostream>
+#include <sstream>
 
 // #define DEBUG_PRINT
+void debugPrint(std::stringstream&);
 
 #endif  // DEFINITIONS_HPP
