cb2a2c4c56aa5f82859252ccc0e85eb02c869ffd
Simon Kärrman <simonkarrman@icloud.com>
2021-02-19 09:38:09 +0100

    Add comment to debug print

--- include/Definitions.hpp
+++ include/Definitions.hpp
@@ -9,6 +9,8 @@
 // TODO: do this with std::format instead as soon as clang adds support for it.
 #define DEBUG_PRINT(...) std::cout << fmt::format(__VA_ARGS__);
 #else
+// Here we trick the compiler to still expand the arguments of the macro. This
+// because it helps us deal with GDB a little bit better
 #define DEBUG_PRINT(...) fmt::format(__VA_ARGS__);
 #endif
 #endif  // DEFINITIONS_HPP
