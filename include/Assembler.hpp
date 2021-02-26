3fee6ef369faff350aaf135e1ab1b25e8ec3def3
Simon Kärrman <simonkarrman@icloud.com>
2021-02-26 14:50:25 +0100

    Update regRegex

--- include/Assembler.hpp
+++ include/Assembler.hpp
@@ -13,7 +13,7 @@
 #include "Definitions.hpp"
 
 static const std::regex regReg(
-    "RDI|RSI|RDX|RCX|R8|R9|R10|R11|RAX|RBX|R12|R13|R14|R15|XMM0|XMM1");
+    "RDI|RSI|RDX|RCX|RAX|RBX|RBP|RSI|R\\d{1,2}|XMM\\d{1,2}");
 static const std::regex lowRegReg("RDI|RSI|RDX|RCX|RAX|RBX|RSP|RBP");
 static const std::regex immReg("^-?[0-9.]+$");
 static const std::regex ptrReg("\\[(R[A-Z0-9]{1,2})((?:\\+|\\-)\\d+)?\\]");
