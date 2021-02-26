17e750ec890336a065d53db5cdf8bf8154334332
Simon Kärrman <simonkarrman@icloud.com>
2021-02-26 10:10:07 +0100

    Make it possible to add comment in asm file

--- test/MixedExecution.asm
+++ test/MixedExecution.asm
@@ -1,13 +1,17 @@
+; Run with 23 2 18
 ENTER 0,0
+; Init variables
 MOV RAX, [RDI+8]
 MOV RAX, [RAX]
-ADD RAX, 6
+; Start of trace (pc = 2)
+ADD RAX, 6 
 ADD RAX, 7
 IMUL RAX, 3
 SUB RAX, 9
 INC RAX
-MOV RDI, [RDI+8]
+; Bailout code
+MOV RDI, [RDI+8] 
 MOV [RDI], RAX
-MOV RAX, 0
+MOV RAX, 0 ; Default exit (pc = 18)
 LEAVE
 RET
