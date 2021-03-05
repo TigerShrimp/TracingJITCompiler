56008df024a99d7a7b06f935f67f33200a2c682a
Jakob Erlandsson <erjakob@student.chalmers.se>
2021-03-05 10:47:55 +0100

    added test

---
+++ test/MoreLoops.java
@@ -0,0 +1,13 @@
+public class MoreLoops {
+  public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
+      System.out.println(1);
+    }
+    for (int i = 0; i < 10; i++) {
+      System.out.println(1);
+    }
+    for (int i = 0; i < 10; i++) {
+      System.out.println(1);
+    }
+  }
+}
