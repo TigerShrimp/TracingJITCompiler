415f3da50f9336529059d3bd45db5eb403156e33
Jakob Erlandsson <erjakob@student.chalmers.se>
2021-05-27 09:33:22 +0200

    changed presentation example

--- test/SingleLoopWithHotSideExit.java
+++ test/SingleLoopWithHotSideExit.java
@@ -1,13 +1,17 @@
 public class SingleLoopWithHotSideExit {
   public static void main(String[] args) {
+    foo();
+  }
+  public static void foo() {
     int i = 0;
     for (int j = 0; j < 10000000; j++) {
       if (j % 100 == 0) {
         i += 2;
-      } else {
+      } else if (j % 50 == 0) {
         i += 1;
+      } else {
+        i += 3;
       }
     }
-    System.out.println(i);
   }
 }
